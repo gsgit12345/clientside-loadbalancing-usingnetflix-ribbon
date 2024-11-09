74cb2ca8aa43dde364474a252a1f33ac0df1654d
gsgit12345 <gsgmca682@gmail.com>
2024-11-09 10:56:35 +0530

    initial commit

--- employee-service/src/main/java/com/example/feignclient/AddressClient.java
+++ employee-service/src/main/java/com/example/feignclient/AddressClient.java
@@ -1,14 +1,13 @@
 package com.example.feignclient;
 
 import com.example.response.AddressResponse;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "address-service", path = "/address-service")
-@RibbonClient(name = "address-service")
+@FeignClient(name = "address-service", url = "http://localhost:8081", path = "/address-service")
+//@RibbonClient(name = "address-service")
 public interface AddressClient {
 
     @GetMapping("/address/{id}")
